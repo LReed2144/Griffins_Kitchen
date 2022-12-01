e9e01cf7d790d691800dbd283ba054fccbb632cd
Lyndsey Reed <43240450+LReed2144@users.noreply.github.com>
2022-11-30 19:35:50 -0800

    login, register, home_page

--- griffinskitchen/urls.py
+++ griffinskitchen/urls.py
@@ -2,5 +2,9 @@ from django.urls import path
 from . import views 
 
 urlpatterns = [
-    path("", views.index, name="index")
+    path("", views.index, name="index"),
+    path("login", views.login_view, name="login"),
+    path("logout", views.logout_view, name="logout"),
+    path("register", views.register, name="register"),
+    path("home_page", views.home_page, name="home_page")
 ]
