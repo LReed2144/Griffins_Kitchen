be0343d3495449541a4c6ae80fc5ea1916ecf1f4
Lyndsey Reed <43240450+LReed2144@users.noreply.github.com>
2022-12-02 12:37:02 -0800

    models update

--- griffinskitchen/models.py
+++ griffinskitchen/models.py
@@ -6,9 +6,13 @@ class User(AbstractUser):
     pass
 
 class Profile(models.Model):
-    user = pass
-    profile_img = models.ImageField()
-    about = pass
-    followers = models.ManyToManyField(User, related_name="get_followed_profiles")
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    id_user = models.IntegerField()
+    profile_img = models.ImageField(upload_to='profile_images', default='blank-profile-picture.png')
+    about = models.TextField(blank=True)
+    # followers = models.ManyToManyField(User, related_name="get_followed_profiles")
 
- 
+    def __str__(self):
+        return self.user.username
+
+    
