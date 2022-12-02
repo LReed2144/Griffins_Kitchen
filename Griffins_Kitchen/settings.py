7c73da0cbbfb535bb98d8e03d6216c90e44e3d94
Lyndsey Reed <43240450+LReed2144@users.noreply.github.com>
2022-12-02 11:59:55 -0800

    media folder

--- Griffins_Kitchen/settings.py
+++ Griffins_Kitchen/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+# importing os to code with this instead of pathlib
 import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -127,3 +128,6 @@ STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
