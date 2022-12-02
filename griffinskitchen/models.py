2164d3186ab685f7fef73bfb661df545ee04200e
Lyndsey Reed <43240450+LReed2144@users.noreply.github.com>
2022-12-02 11:59:59 -0800

    media folder

--- griffinskitchen/models.py
+++ griffinskitchen/models.py
@@ -4,3 +4,11 @@ from django.db import models
 
 class User(AbstractUser):
     pass
+
+class Profile(models.Model):
+    user = pass
+    profile_img = models.ImageField()
+    about = pass
+    followers = models.ManyToManyField(User, related_name="get_followed_profiles")
+
+ 
