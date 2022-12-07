937cb8f15f83febd6357dbc59cc57a9ae3439703
Lyndsey Reed <43240450+LReed2144@users.noreply.github.com>
2022-12-06 22:29:34 -0800

    profile

--- griffinskitchen/urls.py
+++ griffinskitchen/urls.py
@@ -8,9 +8,10 @@ urlpatterns = [
     path("login/", views.login_view, name="login"),
     path("logout/", views.logout_view, name="logout"),
     path("register/", views.register, name="register"),
-    path("home_page/", views.home_page, name="home_page"),
+    path("home_page", views.home_page, name="home_page"),
     path("all-recipes", views.all_recipes, name="all_recipes"),
     path("follow/", views.follow, name="follow"),
     path("follow/", views.follow, name="follow"),
-    path("like_post", views.like_post, name="like_post")
+    path("like_post", views.like_post, name="like_post"),
+    path('profile/<str:pk>', views.profile, name='profile')
 ]
