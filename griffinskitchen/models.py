c9489a098fc6459e6045545b8515e842fca04475
Lyndsey Reed <43240450+LReed2144@users.noreply.github.com>
2022-12-07 19:51:17 -0800

    commit

--- griffinskitchen/models.py
+++ griffinskitchen/models.py
@@ -11,14 +11,30 @@ class Profile(models.Model):
     id_user = models.IntegerField()
     profile_img = models.ImageField(upload_to='profile_images', default='blank-profile-picture.png')
     about = models.TextField(blank=True)
+
+
+    # Relationships
+    # OneToOne: 
+    # ManyToOne: ForeignKey
+    # ManyToMany
+
+    # Reverse Relatinoships
+    # user_posts = Post.objects.get(user=user)
+    # posts = user.posts.all()
+
     # followers = models.ManyToManyField(User, related_name="get_followed_profiles")
+
+
+
 
     def __str__(self):
         return self.user.username
 
+
 class Post(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4)
     user = models.CharField(max_length=100)
+    # user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='posts')
     image = models.ImageField(upload_to='post_images')
     description = models.TextField(blank=True)
     ingredients = models.TextField(blank=True)
@@ -26,6 +42,9 @@ class Post(models.Model):
     created_at = models.DateTimeField(default=datetime.now)
     likes = models.IntegerField(default=0)
     #comments =
+
+    # Relationships
+
 
 
    
@@ -39,3 +58,12 @@ class LikePost(models.Model):
 
     def __str__(self):
         return self.username
+
+
+
+class FollowersCount(models.Model):
+    follower = models.CharField(max_length=100)
+    user = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.user
--- griffinskitchen/views.py
+++ griffinskitchen/views.py
@@ -5,9 +5,10 @@ from django.shortcuts import render, redirect
 from django.urls import reverse
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User, auth
+from itertools import chain
+import random
 
-
-from .models import Profile, Post, User, LikePost
+from .models import Profile, Post, User, LikePost, FollowersCount
 
 
 
@@ -29,12 +30,48 @@ def all_recipes(request):
 
     posts=Post.objects.all()
 
-    return render(request, "griffinskitchen/all_recipes.html",{'user_profile':user_profile, 'posts':posts})
+      # user suggestion starts
+    all_users = User.objects.all()
+    user_following_all = []
+
+    # already following
+    user_following = FollowersCount.objects.filter(follower=request.user.username)
+
+    #get list of already following
+    for user in user_following:
+        user_list = User.objects.get(username=user.user)
+        user_following_all.append(user_list)
+    
+    #not already following
+    # looping through all users not following
+    new_suggestions_list = [x for x in list(all_users) if (x not in list(user_following_all))]
+    # remove self from the list
+    current_user = User.objects.filter(username=request.user.username)
+    # users that are left and randomize
+    final_suggestions_list = [x for x in list(new_suggestions_list) if ( x not in list(current_user))]
+    random.shuffle(final_suggestions_list)
+
+    # create lists
+    username_profile = []
+    username_profile_list = []
+
+    #get ids of left over users
+    for users in final_suggestions_list:
+        username_profile.append(users.id)
+
+    # add ids into a profile list
+    for ids in username_profile:
+        profile_lists = Profile.objects.filter(id_user=ids)
+        username_profile_list.append(profile_lists)
+
+    suggestions_username_profile_list = list(chain(*username_profile_list))
+
+    return render(request, "griffinskitchen/all_recipes.html", {'user_profile':user_profile, 'posts':posts, 'suggestions_username_profile_list': suggestions_username_profile_list[:4]})
 
    
 
-def follow(request):
-    return render(request, "griffinskitchen/follow.html")
+def favorites(request):
+    return render(request, "griffinskitchen/favorites.html")
 
 @login_required
 def upload(request):
@@ -75,7 +112,53 @@ def like_post(request):
 
 @login_required
 def profile(request, pk):
-    return render(request, 'home_page.html')
+    user_object = User.objects.get(username=pk)
+    user_profile = Profile.objects.get(user=user_object)
+    user_posts = Post.objects.filter(user=pk)
+    user_post_length = len(user_posts)
+
+    follower = request.user.username
+    user = pk
+
+    if FollowersCount.objects.filter(follower=follower, user=user).first():
+        button_text = 'Unfollow'
+    else:
+        button_text = 'Follow'
+
+    user_followers = len(FollowersCount.objects.filter(user=pk))
+    user_following = len(FollowersCount.objects.filter(follower=pk))
+
+    context = {
+        'user_object': user_object,
+        'user_profile': user_profile,
+        'user_posts': user_posts,
+        'user_post_length': user_post_length,
+        'button_text': button_text,
+        'user_followers': user_followers,
+        'user_following': user_following,
+    }
+    return render(request, 'griffinskitchen/home_page.html', context)
+    
+
+
+
+@login_required
+def follow(request):
+    if request.method == 'POST':
+        follower = request.POST['follower']
+        user = request.POST['user']
+
+        if FollowersCount.objects.filter(follower=follower, user=user).first():
+            delete_follower = FollowersCount.objects.get(follower=follower, user=user)
+            delete_follower.delete()
+            return redirect('/profile/'+user)
+            # return redirect('/profile/'+user)
+        else:
+            new_follower = FollowersCount.objects.create(follower=follower, user=user)
+            new_follower.save()
+            return redirect('/profile/'+user)
+    else:
+        return redirect('griffinskitchen/all_recipes')
 
 
 @login_required
@@ -105,7 +188,32 @@ def settings(request):
         return redirect('settings')
 
     return render(request, 'griffinskitchen/settings.html', {'user_profile': user_profile})
-   
+
+
+def search(request):
+    #get user and user image
+    user_object = User.objects.get(username=request.user.username)
+    user_profile = Profile.objects.get(user=user_object)
+
+    if request.method == 'POST':
+        #returns any user with letters in the name from the database
+        username = request.POST['username']
+        username_object = User.objects.filter(username__icontains=username)
+
+        username_profile = []
+        username_profile_list = []
+        
+        #get user ids
+        for users in username_object:
+            username_profile.append(users.id)
+
+        for ids in username_profile:
+            profile_lists = Profile.objects.filter(id_user=ids)
+            username_profile_list.append(profile_lists)
+        
+        username_profile_list = list(chain(*username_profile_list))
+    return render(request, 'griffinskitchen/search.html', {'user_profile': user_profile, 'username_profile_list': username_profile_list})
+  
 
 def login_view(request):
     if request.method == "POST":
